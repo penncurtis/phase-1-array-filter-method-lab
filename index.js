cee5b44cbd64f37b36a3bc274ab83e08d6aad7dd
Penn Curtis <penn.s.m.c@gmail.com>
2023-04-11 14:03:56 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver[0] === name[0]) 
+
+}
+
+function matchName(drivers, name){
+    return drivers.filter(match => match.name === name)
+}
+
